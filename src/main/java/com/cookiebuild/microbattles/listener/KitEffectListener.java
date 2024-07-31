74da66ef2152f046c8f2dfbe6ff8e2331bb75221
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-31 08:12:31 +0200

    refactor: KitEffectListener

--- src/main/java/com/cookiebuild/microbattles/listener/KitEffectListener.java
+++ src/main/java/com/cookiebuild/microbattles/listener/KitEffectListener.java
@@ -1,5 +1,3 @@
-// Create a new file: KitEffectListener.java
-
 package com.cookiebuild.microbattles.listener;
 
 import com.cookiebuild.cookiedough.game.Game;
@@ -28,115 +26,94 @@ public class KitEffectListener implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        // get player's game, if it's MicroBattlesGame, get player kit
-        Game game = GameManager.getGameOfPlayer(PlayerManager.getPlayer(player));
-        if (game != null) {
-            if (game instanceof MicroBattlesGame) {
-                Kit kit = ((MicroBattlesGame) game).getKit(PlayerManager.getPlayer(player));
-                if (kit != null) {
-                    switch (kit.getName()) {
-                        case "Ninja":
-                            if (player.isSneaking()) {
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 0));
-                            }
-                            break;
-                        case "Frost Mage":
-                            if (player.getLocation().getBlock().getTemperature() < 0.15) {
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 60, 1));
-                            }
-                            break;
-                    }
+        Kit kit = getPlayerKit(player);
+        if (kit == null) return;
+
+        switch (kit.getName()) {
+            case "Ninja":
+                if (player.isSneaking()) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 0));
                 }
-            }
+                break;
+            case "Frost Mage":
+                if (player.getLocation().getBlock().getTemperature() < 0.15) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 60, 1));
+                }
+                break;
         }
     }
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player attacker && event.getEntity() instanceof Player victim) {
-            CookiePlayer cookieAttacker = PlayerManager.getPlayer(attacker);
-            // get player's game, if it's MicroBattlesGame, get player kit
-            Game game = GameManager.getGameOfPlayer(cookieAttacker);
-            if (game != null) {
-                if (game instanceof MicroBattlesGame) {
-                    Kit kit = ((MicroBattlesGame) game).getKit(cookieAttacker);
-                    if (kit != null) {
-                        switch (kit.getName()) {
-                            case "Vampire":
-                                attacker.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 1));
-                                break;
-                            case "Berserker":
-                                attacker.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 1));
-                                break;
-                        }
-                    }
-                }
-            }
+        if (!(event.getDamager() instanceof Player attacker) || !(event.getEntity() instanceof Player)) return;
+
+        Kit kit = getPlayerKit(attacker);
+        if (kit == null) return;
+
+        switch (kit.getName()) {
+            case "Vampire":
+                attacker.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 1));
+                break;
+            case "Berserker":
+                attacker.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 60, 0));
+                break;
         }
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        // get player's game, if it's MicroBattlesGame, get player kit
-        Game game = GameManager.getGameOfPlayer(PlayerManager.getPlayer(player));
-        if (game != null) {
-            if (game instanceof MicroBattlesGame) {
-                Kit kit = ((MicroBattlesGame) game).getKit(PlayerManager.getPlayer(player));
-                if (kit != null) {
-                    switch (kit.getName()) {
-                        case "Alchemist":
-                            if (event.getItem() != null && event.getItem().getType() == Material.BREWING_STAND) {
-                                // Implement a custom brewing mechanic
-                                // give player a MASS item
-                                player.getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
-                            }
-                            break;
-                        case "Trapper":
-                            if (event.getItem() != null && event.getItem().getType() == Material.TRIPWIRE_HOOK) {
-                                // Implement a trap-setting mechanic
-                                // give player a MASS item
-                                player.getInventory().addItem(new ItemStack(Material.TNT, 1));
+        Kit kit = getPlayerKit(player);
+        if (kit == null) return;
 
-                            }
-                            break;
-                    }
+        ItemStack item = event.getItem();
+        if (item == null) return;
+
+        switch (kit.getName()) {
+            case "Alchemist":
+                if (item.getType() == Material.BREWING_STAND) {
+                    player.getInventory().addItem(new ItemStack(Material.POTION, 1));
                 }
-            }
+                break;
+            case "Trapper":
+                if (item.getType() == Material.TRIPWIRE_HOOK) {
+                    player.getInventory().addItem(new ItemStack(Material.TNT, 1));
+                }
+                break;
         }
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
-        CookiePlayer cookiePlayer = PlayerManager.getPlayer(player);
-        // get player's game, if it's MicroBattlesGame, get player kit
-        Game game = GameManager.getGameOfPlayer(cookiePlayer);
-        if (game != null) {
-            if (game instanceof MicroBattlesGame) {
-                Kit kit = ((MicroBattlesGame) game).getKit(cookiePlayer);
-                if (kit != null && kit.getName().equals("Juggernaut")) {
-                    // Implement a death explosion for Juggernaut
-                    player.getWorld().createExplosion(player.getLocation(), 2.0f, false, false);
-                }
-            }
-        }
+        Kit kit = getPlayerKit(player);
+        if (kit == null) return;
 
+        if (kit.getName().equals("Juggernaut")) {
+            player.getWorld().createExplosion(player.getLocation(), 2.0f, false, false);
+        }
     }
 
     @EventHandler
     public void onProjectileHit(ProjectileHitEvent event) {
-        if (event.getEntity() instanceof Arrow && event.getEntity().getShooter() instanceof Player shooter) {
-            CookiePlayer cookiePlayer = PlayerManager.getPlayer(shooter);
-            Game game = GameManager.getGameOfPlayer(cookiePlayer);
+        if (!(event.getEntity() instanceof Arrow) || !(event.getEntity().getShooter() instanceof Player shooter))
+            return;
 
-            if (game instanceof MicroBattlesGame && game.hasStarted()) {
-                Kit kit = ((MicroBattlesGame) game).getKit(cookiePlayer);
-                if (kit != null && kit.getName().equals("Explosive Archer")) {
-                    Location loc = event.getEntity().getLocation();
-                    loc.getWorld().createExplosion(loc, 1.0F, true, true);
-                }
-            }
+        Kit kit = getPlayerKit(shooter);
+        if (kit == null) return;
+
+        if (kit.getName().equals("Explosive Archer")) {
+            Location loc = event.getEntity().getLocation();
+            loc.getWorld().createExplosion(loc, 1.0F, true, true);
         }
+    }
+
+    private Kit getPlayerKit(Player player) {
+        CookiePlayer cookiePlayer = PlayerManager.getPlayer(player);
+        Game game = GameManager.getGameOfPlayer(cookiePlayer);
+        if (!(game instanceof MicroBattlesGame microBattlesGame) || !game.hasStarted()) {
+            return null;
+        }
+        return microBattlesGame.getKit(cookiePlayer);
     }
 }
